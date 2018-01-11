3dd90c65934a9c84672788c05ca50efa027cb9be
Hui Wen <huiwen_28@hotmail.com>
2018-01-11 15:14:52 +0800

    Encrypt passwords

--- store.js
+++ store.js
@@ -1,11 +1,28 @@
+const crypto = require("crypto");
 const knex = require("knex")(require("./knexfile"));
 
 module.exports = {
+  saltHashPassword,
   createUser({ username, password }) {
     console.log(`Add user ${username} with password ${password}`);
+    const { salt, hash } = saltHashPassword(password);
     return knex("user").insert({
-      username,
-      password
+      salt,
+      encrypted_password: hash,
+      username
     });
   }
 };
+
+function saltHashPassword(password) {
+  const salt = randomString();
+  const hash = crypto.createHmac("sha512", salt).update(password);
+  return {
+    salt,
+    hash: hash.digest("hex")
+  };
+}
+
+function randomString() {
+  return crypto.randomBytes(4).toString("hex");
+}
